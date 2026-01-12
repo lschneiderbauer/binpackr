039d22ca005f33e1211f29dbe67e1b8a68fb5c2b
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2026-01-12 19:59:28 +0100

    update docs

--- R/bin_pack.R
+++ R/bin_pack.R
@@ -1,22 +1,24 @@
 #' 1D bin packing "First Fit (Decreasing)" algorithm
 #'
-#' @param x A numeric vector of item sizes to be fit into bins. Each value
+#' @param x [[numeric()]] A numeric vector of item sizes to be fit into bins. Each value
 #'          represents the size of an atomic item. If a value is `NA` it is
 #'          ignored and the corresponding result will also be `NA`.
-#' @param cap Bin capacity in units of values in `x`. A scalar value. If
-#'            an individual item size is above `cap` a single bin is reserved
+#' @param cap \[`numeric(1)`\] A scalar value representing the bin capacity in units of values in `x`.
+#'            If an individual item size is above `cap` a single bin is reserved
 #'            for this item.
-#' @param sort Determines whether the input vector should be sorted in
+#' @param sort \[`logical(1)`\] Determines whether the input vector should be sorted in
 #'             decreasing order before applying the "First Fit" algorithm
 #'             ("First Fit Decreasing").
-#' @returns An integer vector of labels of the same length as `x`. The integer
+#' @returns [[integer()]] An integer vector of labels of the same length as `x`. The integer
 #'          label at position `i` determines the assignment of the `i`th item
 #'          with size `x[i]` to a bin. If the value `x[i]` is `NA` the result
 #'          label at position `i` will also be `NA`.
-#' @details See [Wikipedia](https://en.wikipedia.org/wiki/First-fit_bin_packing)
-#'          for a concise introduction or
-#'          "The Art of Computer Programming Vol. 1" by Donald E. Knuth
-#'          (1997, ISBN: 0201896834) for more details.
+#' 
+#' @details
+#'   See [Wikipedia](https://en.wikipedia.org/wiki/First-fit_bin_packing)
+#'   for a concise introduction or
+#'   "The Art of Computer Programming Vol. 1" by Donald E. Knuth
+#'   (1997, ISBN: 0201896834) for more details.
 #' @examples
 #' # Generate a vector of item sizes
 #' x <- sample(100, 1000, replace = TRUE)
